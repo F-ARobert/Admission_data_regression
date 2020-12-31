0b391c76803dfaba94ed59933e13989f331abb2b
F-ARobert <felix-antoine.robert.1@etsmtl.net>
2020-12-30 22:44:50 -0500

    Devide data into features and labels

--- main.py
+++ main.py
@@ -14,6 +14,25 @@ from sklearn.preprocessing import Normalizer
 from sklearn.metrics import r2_score
 
 # Import data
-admission_data = pd.read_csv("admissions_data.csv")
+dataset = pd.read_csv("admissions_data.csv")
 # print(admission_data.head())
+
+# Select features and labels
+# Serial No. (col 0) is not to be included in features.
+dataset = dataset.drop(['Serial No.'], axis=1)  # removes serial numbers from the data
+# print(admission_data.head())
+
+# select labels
+labels = dataset.iloc[:-1]
+
+# Features are from col. 1 to 7
+features = dataset.iloc[:, 0:-1]  # Select all rows from all columns save last
+#print(features.head())
+#print(labels.describe())
+
+# Split training data and test data
+# Because we don't have a lot of data, we should use k-fold validation
+# Todo implement k-fold data validation.
+
+
 
